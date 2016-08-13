14b637ec5bf82bd0e53b700d178f8954e530b8f1
Rajni <rajani.ashok63@gmail.com>
2016-08-13 18:24:13 +0530

    version2

--- src/jenkinpackage/Test1.java
+++ src/jenkinpackage/Test1.java
@@ -4,19 +4,46 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 
 public class Test1 {
 	
+	ExtentReports c = new ExtentReports("./Result/ExtentReport.html");
 	@Test
 	public void TestA()
 	{
 	
-		
-		//System.setProperty("webdriver.gecko.driver","C:/JenkinsWorkSpace/Jenkin/exe/geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
-		driver.get("https://www.google.co.in/");
-		driver.close();
-				
 	
+	ExtentTest t = c.startTest("t1");
+	t.log(LogStatus.PASS, "Hi");
+		//System.setProperty("webdriver.gecko.driver","C:/JenkinsWorkSpace/Jenkin/exe/geckodriver.exe");
+     WebDriver driver = new FirefoxDriver();
+	 driver.get("https://www.google.co.in/");
+     driver.close();
+				
+	c.endTest(t);
+	//c.flush(); // This will write into result window 
 	}
+	
+	@Test
+	public void TestB()
+	{
+	
+	//ExtentReports c = new ExtentReports("./Result/ExtentReport.html");
+	ExtentTest t = c.startTest("t2");
+	t.log(LogStatus.FAIL, "fail");
+		//System.setProperty("webdriver.gecko.driver","C:/JenkinsWorkSpace/Jenkin/exe/geckodriver.exe");
+     WebDriver driver = new FirefoxDriver();
+	 driver.get("https://www.google.co.in/");
+     driver.close();
+				
+	c.endTest(t);
+	c.flush(); // This will write into result window 
+	}
+	
+	
+	
 }
